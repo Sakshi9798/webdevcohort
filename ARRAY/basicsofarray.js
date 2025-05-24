326065bcf54c827acb1f964430d5999d3270edda
Sakshi9798 <sakshiparjapat1709@gmail.com>
2025-05-24 22:27:50 +0530

    added to webdevcohort

---
+++ ARRAY/basicsofarray.js
@@ -0,0 +1,13 @@
+// const arr=[44,5, "alphaintern"] //using array literal
+// const arr1=new Array(4,5,"alpha") //using new key word to create array
+
+const arr3=["alphaintern",55,
+    function hello() {console.log("hii")},
+    {task1: 'exercise'}
+];
+
+arr3[2]();
+
+console.log(arr3[0]);
+
+console.log(arr3[3].task1);
